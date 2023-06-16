685445cc844388a915a4539554c0df6c31dbd55b
nathanriveraQA <nathanriveramgr@gmail.com>
2023-06-16 12:41:51 -0400

    update-branch

--- Python/GenerateJSONPayloadFromSchema/payload.py
+++ Python/GenerateJSONPayloadFromSchema/payload.py
@@ -1,3 +1,4 @@
+
 import json 
 import random 
 import random, string
@@ -81,20 +82,43 @@ value ={
         "age": (random.randint(18,50)),  
         "accountNumber": (random.randint(999,9999))
         },
-    
+    #
 }  
 # the json file to saves the output data   
-save_file = open("saveHIGdata.json", "w")  
+save_file = open("C:\Temp\data.json", "w")  
 json.dump(value, save_file, indent = 6)  
 save_file.close()  
-def writeToJSONFile(path, fileName, data):
-    filePathNameWExt =  path + '/' + fileName + '.json'
-    with open(filePathNameWExt, 'a') as fp:
-        json.dump(data, fp)
-        fp.write(data)
+df = pd.read_json('C:\Temp\data.json')
+print(df.to_string())
 
-writeToJSONFile("C:\Temp", "payloadPython", "payloadData")
+
+
+# def writeToJSONFile(path, fileName, data):
+#     filePathNameWExt =  path + '/' + fileName + '.json'
+#     with open(filePathNameWExt, 'a') as fp:
+#         json.dump(data, fp)
+#         fp.write(data)
+# def writeToJSONFile("C:\Temp", "payloadPython", data):
+#     with open("C:\Temp", 'a') as fp:
+#         json.dump(data, fp)
+#         fp.write(data)
+        
+        
 # df = pd.read_json('data.json')
 #
 # print(df.to_string())
 #
+
+# filename = 'saveHIGdata.json'
+# entry = {'carl': 33}
+
+# # 1. Read file contents
+# with open("C:\Temp\saveHIGdata.json", "r") as file:
+#     data = json.load(file)
+
+# # 2. Update json object
+# data.append(entry)
+
+# # 3. Write json file
+# with open("C:\Temp\saveHIGdata.json", "w") as file:
+#     json.dump(data, file)
